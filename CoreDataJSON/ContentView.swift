a33b6074acb49d886123aae3c564da37cd6816ab
FranicevicNikola <nikola007f@gmail.com>
2020-09-17 07:12:08 +0200

    Initial Commit

---
+++ CoreDataJSON/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CoreDataJSON
+//
+//  Created by Master Family on 17/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
